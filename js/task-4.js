bcf284bed3753f0e674eb910bdac9e999d19b437
Lakanna <annalakus@ukr.net>
2024-07-22 23:47:34 +0100

    22

--- js/task-4.js
+++ js/task-4.js
@@ -1,28 +1,34 @@
-
-const loginForm = document.querySelector(".login-form")
+const loginForm = document.querySelector(".login-form");
 
 console.dir(loginForm);
 
-loginForm.addEventListener('submit', handlerSubmit);
+loginForm.addEventListener("submit", handlerSubmit);
 
-function handlerSubmit(ev){
-    ev.preventDefault();
+function handlerSubmit(ev) {
+  ev.preventDefault();
 
-    const form = ev.target;
-    const emailEl = form.elements.email.value;
-    const passwordEl = form.elements.password.value;
+  const form = ev.target;
+  const emailEl = form.elements.email.value;
+  const passwordEl = form.elements.password.value;
 
-    if(emailEl ===""||passwordEl=== ""){
-        alert("All form fields must be filled in");
-        return;
-    };
+  if(emailEl ===""||passwordEl=== ""){
+      alert("All form fields must be filled in");
+      return;
+  };
 
-    const result = {
-        email: emailEl.trim(),
-        password: passwordEl.trim()
-    };
+  // const result = {
+  //     email: emailEl.trim(),
+  //     password: passwordEl.trim()
+  // };
 
-    console.log(result);
+  // console.log(result);
 
-    loginForm.reset();
+  let data = new FormData(event.currentTarget);
+  console.dir(data);
+
+  let result = {};
+  data.forEach((value, key) => (result[key] = value.trim()));
+
+  console.log(result);
+  loginForm.reset();
 }
